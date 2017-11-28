c051bc70dbddca0e4b1e5c2c881ec2c475662115
ArtCouSan <artcousan@gmail.com>
2017-11-27 22:02:07 -0200

    Cadastro Cliente Funcionando

--- ProjetoIntegrador/src/main/java/br/senac/pi4/ProjetoIntegrador/Service/ClienteService.java
+++ ProjetoIntegrador/src/main/java/br/senac/pi4/ProjetoIntegrador/Service/ClienteService.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.senac.pi4.ProjetoIntegrador.Service;
 
 import br.senac.pi4.ProjetoIntegrador.entity.Cliente;
 import java.io.Serializable;
 import java.util.List;
 
-/**
- *
- * @author jonat
- */
+
 public interface ClienteService extends Serializable {
     
     public List<Cliente> listar(int offset);
