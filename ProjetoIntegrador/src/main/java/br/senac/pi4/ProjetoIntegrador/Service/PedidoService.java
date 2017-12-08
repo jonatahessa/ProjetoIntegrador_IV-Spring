6d400f315d24a33a1105f96e882dcdee0a09577b
jonatahessa <jonata.hessa@gmail.com>
2017-12-08 19:14:24 -0200

    droga

---
+++ ProjetoIntegrador/src/main/java/br/senac/pi4/ProjetoIntegrador/Service/PedidoService.java
@@ -0,0 +1,22 @@
+
+package br.senac.pi4.ProjetoIntegrador.Service;
+
+import br.senac.pi4.ProjetoIntegrador.entity.Pedido;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+
+public interface PedidoService extends Serializable {
+    
+    public List<Pedido> listar(int offset, int quantidade);
+       
+    public Pedido obter(Long codigoPedido);
+    
+    public void incluir(Pedido pedido);
+    
+    public void alterar(Pedido pedido);
+    
+    public List<Pedido> pesquisarDatas(Date dataDe, Date dataAte);
+            }
+
